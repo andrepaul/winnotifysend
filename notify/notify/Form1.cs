2084294bdf216f5caea35781edbb55707692d46c
André Paul <andre.paul@fokus.fraunhofer.de>
2013-02-26 11:54:19 +0100

    initial source commit

---
+++ notify/notify/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using notify.Properties;
+using System.IO;
+using System.Resources;
+using System.Drawing.Drawing2D;
+using System.Media;
+/*******************************************************************************
+ *  Code contributed to the webinos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2012-2013 André Paul
+ *
+ ******************************************************************************/
+namespace notify
+{
+    public partial class Form1 : Form
+    {
+        Thread workerThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            newAlert();
+
+
+            
+            workerThread = new Thread(DoWork);
+            workerThread.Start();
+
+            this.TopMost = true;
+            this.Focus();
+            this.BringToFront();
+
+        }
+
+        private void newAlert()
+        {
+
+            
+
+            SystemSounds.Beep.Play();
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length == 1)
+            {
+                this.labelNotification.Text = "Usage: notify-send <\"Headline\"> <\"body\"> -i <warntype> where warntype is absolute file uri";
+            }
+
+            
+            int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+
+            this.Location = new Point(screenWidth - 350, 50);
+
+            foreach (string arg in Environment.GetCommandLineArgs())
+            {
+                Console.Out.WriteLine("Log: " + arg);
+            }
+
+            if (args.Length >= 3) labelNotification.Text = args[2];
+            if (args.Length >= 2) label1.Text = args[1];
+
+
+            if (args.Length >= 5 && args[3] == "-i")
+            {
+                Image img = null;
+
+                if (File.Exists(args[4]))
+                {
+
+                    img = new Bitmap(args[4]);
+
+                    if (img != null)
+                    {
+                        img = resizeImage(img, new Size(51, 51));
+                        this.pictureBox1.Image = img;
+                    }
+                }
+            }
+        }
+
+ 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+ 
+        public void DoWork()
+        {
+           Thread.Sleep(7000);
+           Console.Out.WriteLine("CLOSED");
+           Application.Exit();
+        }
+
+        private static Image resizeImage(Image imgToResize, Size size)
+        {
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (Image)b;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("CLICKED");
+            workerThread.Abort();
+            Application.Exit();
+        }
+
+        private void labelNotification_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("CLICKED");
+            workerThread.Abort();
+            Application.Exit();
+        }
+
+
+    }
+}
---
+++ notify/notify/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("notify")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Fraunhofer Institut FOKUS")]
+[assembly: AssemblyProduct("notify")]
+[assembly: AssemblyCopyright("Copyright © Fraunhofer Institut FOKUS 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3e37b5e-cb89-477a-abb8-7162ccdeb5c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
